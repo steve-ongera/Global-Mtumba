3891f0544c4a870ccbe432186b08371c2511d120
steve-ongera <gadafisteve001@gmail.com>
2025-01-08 12:42:43 +0300

    added new fields in product model

--- product/models.py
+++ product/models.py
@@ -26,6 +26,12 @@ class Product(models.Model):
     tags = TaggableManager()
     slug = models.SlugField(null=True , blank=True )
 
+    order_count = models.IntegerField(default=0)
+    rating = models.FloatField(default=0.0 , null=True , blank=True)
+    discount = models.FloatField(default=0.0 , null=True , blank=True)
+    original_price = models.DecimalField(max_digits=10, decimal_places=2 , null=True , blank=True)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2 , null=True , blank=True)
+
     def __str__(self):
         return self.name
     
